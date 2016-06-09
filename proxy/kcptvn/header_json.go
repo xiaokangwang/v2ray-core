1dad00ffedc715fa274f218ede551e43c99006c0
Shelikhoo <xiaokangwang@outlook.com>
2016-06-09 16:29:50 +0800

    Added Support for UDP traffic

--- proxy/kcptvn/header_json.go
+++ proxy/kcptvn/header_json.go
@@ -12,19 +12,26 @@ func decodeDestJSON(input io.Reader) v2net.Destination {
 	var dest jsonDest
 	decoder.Decode(&dest)
 	p, _ := v2net.PortFromInt(dest.Port)
-	destv2 := v2net.TCPDestination(v2net.ParseAddress(dest.Address), p)
+	if dest.Net == "tcp" {
+		destv2 := v2net.TCPDestination(v2net.ParseAddress(dest.Address), p)
+		return destv2
+	}
+	destv2 := v2net.UDPDestination(v2net.ParseAddress(dest.Address), p)
 	return destv2
+
 }
 
 type jsonDest struct {
 	Address string `json:"Address"`
 	Port    int    `json:"Port"`
+	Net     string `json:"Network"`
 }
 
 func encodeDestJSON(dest v2net.Destination, output io.Writer) {
 	var destj jsonDest
 	destj.Address = dest.Address().String()
 	destj.Port = int(dest.Port().Value())
+	destj.Net = string(dest.Network())
 	encoder := json.NewEncoder(output)
 	encoder.Encode(destj)
 }

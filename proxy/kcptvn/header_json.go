af6af16fe55f30d9c911657636f6072dd696fb6f
Shelikhoo <xiaokangwang@outlook.com>
2016-06-09 15:56:09 +0800

    Fixed: kcptun passing reference to JSON decode

--- proxy/kcptvn/header_json.go
+++ proxy/kcptvn/header_json.go
@@ -10,7 +10,7 @@ import v2net "github.com/v2ray/v2ray-core/common/net"
 func decodeDestJSON(input io.Reader) v2net.Destination {
 	decoder := json.NewDecoder(input)
 	var dest jsonDest
-	decoder.Decode(dest)
+	decoder.Decode(&dest)
 	p, _ := v2net.PortFromInt(dest.Port)
 	destv2 := v2net.TCPDestination(v2net.ParseAddress(dest.Address), p)
 	return destv2
